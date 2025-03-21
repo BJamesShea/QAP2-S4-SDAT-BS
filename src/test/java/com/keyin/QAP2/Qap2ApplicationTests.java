5d9ee22a8ceea1c2f97e121535ed1166136aec16
Brandon Shea <brandon.shea1994@icloud.com>
2025-03-21 11:01:20 -0230

    Initial commit

---
+++ src/test/java/com/keyin/QAP2/Qap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.QAP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
