acd6109761e5cd6d70d42edc397207cae415e4be
Brandon Shea <brandon.shea1994@icloud.com>
2025-03-23 20:40:37 -0230

    TournamentService wiring

--- src/main/java/com/keyin/QAP2/service/TournamentService.java
+++ src/main/java/com/keyin/QAP2/service/TournamentService.java
@@ -1,4 +1,51 @@
 package com.keyin.QAP2.service;
 
+import com.keyin.QAP2.model.Tournament;
+import com.keyin.QAP2.repository.TournamentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TournamentService {
+
+
+    @Autowired
+    private TournamentRepository tournamentRepository;
+
+    public List<Tournament> getAllTournaments(){
+        return tournamentRepository.findAll();
+    }
+
+    public Optional<Tournament> getTournamentById(Long id){
+        return tournamentRepository.findById(id);
+    }
+
+    public Tournament addTournament(Tournament tournament){
+        return tournamentRepository.save(tournament);
+    }
+
+    public Tournament updateTournament(Long id, Tournament updatedTournament){
+        Optional<Tournament> existingTournamentOpt = tournamentRepository.findById(id);
+
+        if(existingTournamentOpt.isPresent()){
+            Tournament existingTournament = existingTournamentOpt.get();
+            existingTournament.setLocation(updatedTournament.getLocation());
+            existingTournament.setStartDate(updatedTournament.getStartDate());
+            existingTournament.setEndDate(updatedTournament.getEndDate());
+            existingTournament.setEntryFee(updatedTournament.getEntryFee());
+            existingTournament.setCashPrize(updatedTournament.getCashPrize());
+
+            return tournamentRepository.save(existingTournament);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteTournament(Long id){
+        tournamentRepository.deleteById(id);
+    }
+
 }
