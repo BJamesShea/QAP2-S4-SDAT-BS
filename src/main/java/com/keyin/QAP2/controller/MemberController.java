b726ffd259ebf90c61ed4814f657b1786740648b
Brandon Shea <brandon.shea1994@icloud.com>
2025-03-23 18:31:06 -0230

    application.properties config & MemberController wiring

--- src/main/java/com/keyin/QAP2/controller/MemberController.java
+++ src/main/java/com/keyin/QAP2/controller/MemberController.java
@@ -1,4 +1,29 @@
 package com.keyin.QAP2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.keyin.QAP2.service.MemberService;
+import com.keyin.QAP2.model.Member;
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
 public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    @GetMapping("/{id}")
+    public Member getMemberById(@PathVariable Long id) {
+        return memberService.getMemberById(id).orElse(null);
+    }
+
+
+
+
 }
